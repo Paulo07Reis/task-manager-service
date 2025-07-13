e59f23bc04710b1c7b6c041d4febda313bc1782f
Paulo07Reis <pmarcena21@gmail.com>
2025-07-13 20:12:04 -0300

    feat: Creating newTask function on Task entity

--- src/main/java/app/domain/entities/Task.java
+++ src/main/java/app/domain/entities/Task.java
@@ -1,6 +1,7 @@
 package app.domain.entities;
 
 import app.domain.enums.Status;
+import app.domain.services.TaskService;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,4 +14,9 @@ public class Task {
     private String description;
     private int priority;
     private Status status;
+
+    public Task newTask(){
+        TaskService.tasks.add(this);
+        return this;
+    }
 }
