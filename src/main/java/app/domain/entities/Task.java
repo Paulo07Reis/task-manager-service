2c4d24a54490221808561e674dc8e00997b0ed44
Paulo07Reis <pmarcena21@gmail.com>
2025-07-13 20:37:40 -0300

    feat: Implementing builder concept on Task entity

--- src/main/java/app/domain/entities/Task.java
+++ src/main/java/app/domain/entities/Task.java
@@ -3,10 +3,12 @@ package app.domain.entities;
 import app.domain.enums.Status;
 import app.domain.services.TaskService;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Task {
@@ -15,8 +17,64 @@ public class Task {
     private int priority;
     private Status status;
 
+    public Task(Builder builder) {
+        this.title = builder.title;
+        this.description = builder.description;
+        this.priority = builder.priority;
+        this.status = builder.status;
+    }
+
     public Task newTask(){
         TaskService.tasks.add(this);
         return this;
+    }
+
+    public static Builder builder(){
+        return new Builder();
+    }
+
+    public static Builder builderFrom(Task task){
+        return new Builder(task);
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Builder {
+        private String title;
+        private String description;
+        private int priority;
+        private Status status;
+
+        public Builder(Task task) {
+            this.title = task.title;
+            this.description = task.description;
+            this.priority = task.priority;
+            this.status = task.status;
+        }
+
+        public Builder withTitle(String title){
+            this.title = title;
+            return this;
+        }
+
+        public Builder withDescription(String description){
+            this.description = description;
+            return this;
+        }
+
+        public Builder withPriority(int priority){
+            this.priority = priority;
+            return this;
+        }
+
+        public Builder withState(Status status){
+            this.status = status;
+            return this;
+        }
+
+        public Task build(){
+            return new Task(this);
+        }
     }
 }
