0967bd71f25cb89ce01e97d19c0e88299085f45c
Paulo07Reis <pmarcena21@gmail.com>
2025-07-14 17:21:37 -0300

    feat: Creating a converter to Task entities

---
+++ src/main/java/app/application/converter/TaskConverter.java
@@ -0,0 +1,45 @@
+package app.application.converter;
+
+import app.application.dtos.TaskDto;
+import app.domain.entities.Task;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Component
+public class TaskConverter {
+
+    public Task toDomain(TaskDto dto){
+        return Optional.ofNullable(dto)
+                .map(source -> Task.builder()
+                        .withTitle(source.getTitle())
+                        .withDescription(source.getDescription())
+                        .withPriority(source.getPriority())
+                        .withState(source.getStatus())
+                        .build()
+                )
+                .orElse(null);
+    }
+
+    public TaskDto toDto(Task task){
+        return Optional.ofNullable(task)
+                .map(source -> {
+                    TaskDto dto = new TaskDto();
+                    dto.setTitle(source.getTitle());
+                    dto.setDescription(source.getDescription());
+                    dto.setPriority(source.getPriority());
+                    dto.setStatus(source.getStatus());
+                    return dto;
+                })
+                .orElse(null);
+    }
+
+    public List<TaskDto> convertList(List<Task> list){
+        return Optional.ofNullable(list)
+                .map(array -> array.stream().map(this::toDto).collect(Collectors.toList()))
+                .orElse(new ArrayList<>());
+    }
+}
