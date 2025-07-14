606996b8f965c3b6f38f4389d11ce8607ed8c92d
Paulo07Reis <pmarcena21@gmail.com>
2025-07-14 17:21:08 -0300

    feat:Implementing DTOs concept on the controller

--- src/main/java/app/application/controllers/TaskController.java
+++ src/main/java/app/application/controllers/TaskController.java
@@ -1,5 +1,7 @@
 package app.application.controllers;
 
+import app.application.converter.TaskConverter;
+import app.application.dtos.TaskDto;
 import app.domain.entities.Task;
 import app.domain.services.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,15 +16,18 @@ public class TaskController {
     @Autowired
     private TaskService service;
 
+    @Autowired
+    private TaskConverter converter;
+
     @GetMapping
-    public Mono<List<Task>> getTasks() {
-        return service.listTasks();
+    public Mono<List<TaskDto>> getTasks() {
+        return service.listTasks().map(converter::convertList);
     }
 
     @PostMapping
-    public Mono<Task> createTak(
+    public Mono<TaskDto> createTak(
             @RequestBody Task task
     ){
-        return service.insertTask(task);
+        return service.insertTask(task).map(converter::toDto);
     }
 }
