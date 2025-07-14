c31e499f5efd8ccde5c033d52b41bb6dfeaf9f7b
Paulo07Reis <pmarcena21@gmail.com>
2025-07-13 22:14:05 -0300

    feat: Creating a controller to tasks with two method get and post

---
+++ src/main/java/app/application/controllers/TaskController.java
@@ -0,0 +1,30 @@
+package app.application.controllers;
+
+
+import app.domain.entities.Task;
+import app.domain.services.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/task")
+public class TaskController {
+
+    @Autowired
+    private TaskService service;
+
+    @GetMapping
+    public Mono<List<Task>> getTasks() {
+        return service.listTasks();
+    }
+
+    @PostMapping
+    public Mono<Task> createTak(
+            @RequestBody Task task
+    ){
+        return service.insertTask(task);
+    }
+}
