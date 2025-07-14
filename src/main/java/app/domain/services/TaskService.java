4700753b3107dd469f8155de54ac0966dbcde0f1
Paulo07Reis <pmarcena21@gmail.com>
2025-07-13 22:13:09 -0300

    feat: Initializing task list on task service

--- src/main/java/app/domain/services/TaskService.java
+++ src/main/java/app/domain/services/TaskService.java
@@ -3,13 +3,13 @@ package app.domain.services;
 import app.domain.entities.Task;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
-
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class TaskService {
 
-    public static List<Task> tasks;
+    public static List<Task> tasks = new ArrayList<>();
 
     public Mono<Task> insertTask(Task task){
         return Mono.just(task)
