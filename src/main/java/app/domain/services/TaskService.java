7c17d2a648a7b71be1ea3e57e78742e1f5281b0b
Paulo07Reis <pmarcena21@gmail.com>
2025-07-16 15:05:27 -0300

    feat: Implementing Pageable and custom repository on service task

--- src/main/java/app/domain/services/TaskService.java
+++ src/main/java/app/domain/services/TaskService.java
@@ -3,8 +3,11 @@ package app.domain.services;
 import app.domain.entities.Task;
 import java.util.ArrayList;
 import java.util.List;
+
+import app.resource.repositories.TaskCustomRepository;
 import app.resource.repositories.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
@@ -16,14 +19,17 @@ public class TaskService {
     @Autowired
     private TaskRepository repository;
 
+    @Autowired
+    private TaskCustomRepository customRepository;
+
     public Mono<Task> insertTask(Task task){
         return Mono.just(task)
                 .map(Task::insert)
                 .flatMap(this::save);
     }
 
-    public Mono<List<Task>> listTasks(){
-        return Mono.just(repository.findAll());
+    public Page<Task> findPaginated(Task task, Integer pageNumber, Integer pageSize){
+        return customRepository.findPaginated(task, pageNumber, pageSize);
     }
 
     private Mono<Task> save(Task task){
